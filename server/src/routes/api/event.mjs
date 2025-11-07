364cf43273d69eb31ede547e91b59f183b35adc9
Tomasz Blaszczak <tomaszblaszczak.w2@outlook.com>
2025-11-07 17:34:01 +0100

    feat: set method name for events without "on"

--- server/src/routes/api/event.mjs
+++ server/src/routes/api/event.mjs
@@ -30,7 +30,11 @@ import * as events from '../../events.mjs';
 function sendEvent(req, res) {
   const { ws } = res.locals; // Like magic!
   const userId = getUserIdFromReq(req);
-  const { method, result } = req.body;
+  let { method, result } = req.body;
+  let [ moduleN, methodN ] = method.split('.');
+  if (!methodN.startsWith('on') && methodN.endsWith('Changed')) {
+    method = `${moduleN}.on${methodN.charAt(0).toUpperCase() + methodN.slice(1)}`;
+  }
 
   function fSuccess() {
     res.status(200).send({
